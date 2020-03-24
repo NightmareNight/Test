807262893b9bca4ca993284b86390657a57caa54
Anastasia <moroznastia20202@mail.ru>
2020-03-24 11:50:37 +0300

    Test 2

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -14,30 +14,38 @@ export class ProductsComponent implements OnInit {
 
   ProductName: '';
   ProductPrice: 0;
-  ProductVal: 1;
+  ProductAmount: 1;
 
 
   ngOnInit() {
   }
 
   Products = [
-    {name: "Картошка", price: 20, val: 10},
-    {name: "Абрикос", price: 30, val: 30},
-    {name: "Ананас", price: 50, val: 200},
-    {name: "Виски", price: 300, val: 1}
+    {name: "Картошка", price: 20, amount: 10},
+    {name: "Абрикос", price: 30, amount: 30},
+    {name: "Ананас", price: 50, amount: 200},
+    {name: "Виски", price: 300, amount: 1}
     ];
 
     
 
   AddProduct() {
-    this.Products.push ({name: this.ProductName, price: this.ProductPrice, val: this.ProductVal});
+    this.Products.push ({name: this.ProductName, price: this.ProductPrice, amount: this.ProductAmount});
     this.ProductName = '';
     }
 
-  AddKorzina(name, price, val) {
-    this.productVKorzine.Products.push ({name: name, price: price, val:  +1});
+  AddKorzina(name, price) {
     let product = this.Products.find(product => product.name === name);
-    product.val = product.val - 1;
+    product.amount = product.amount - 1;
+
+    let productInBusket = this.productVKorzine.Products.find(x => x.name === name);
+    if(productInBusket === undefined){
+      this.productVKorzine.Products.push ({name: name, price: price, amount: 1});
+    }
+    else{
+      productInBusket.amount = productInBusket.amount + 1;
+    }
+   
     
   }
 
